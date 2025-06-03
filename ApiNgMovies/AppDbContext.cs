1999f98a9fb209ba6721e6c958934c00616ea33e
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-02 20:46:15 -0500

    Agregado soporte para Entity Framework Core y SQL Server

---
+++ ApiNgMovies/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiNgMovies
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+    }
+}
--- ApiNgMovies/Program.cs
+++ ApiNgMovies/Program.cs
@@ -1,4 +1,5 @@
 using ApiNgMovies;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddOutputCache(options =>
 {
